9e109bb662289135845c542cae973c3b96fbe071
Dani Santos <santos.adaniele@gmail.com>
2020-03-31 15:26:34 -0400

    fetch teweets by Id

--- client/src/components/CurrentUserContext.js
+++ client/src/components/CurrentUserContext.js
@@ -5,17 +5,30 @@ export const CurrentUserContext = React.createContext(null);
 export const CurrentUserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = React.useState(null);
   const [status, setStatus] = React.useState("Loading");
+  const [feed, setFeed] = React.useState(null);
+  const [feedStatus, setFeedStatus] = React.useState("Loading");
 
   useEffect(() => {
-    fetch("http://localhost:31415/api/me/profile")
+    fetch("/api/me/profile")
       .then(res => res.json())
       .then(data => {
         setCurrentUser(data.profile);
         setStatus("ok");
       });
   }, []);
+
+  useEffect(() => {
+    fetch("/api/me/home-feed")
+      .then(res => res.json())
+      .then(data => {
+        setFeed(Object.values(data.tweetsById));
+        setFeedStatus("ok");
+      });
+  }, []);
   return (
-    <CurrentUserContext.Provider value={{ currentUser, status }}>
+    <CurrentUserContext.Provider
+      value={{ currentUser, status, feed, feedStatus }}
+    >
       {children}
     </CurrentUserContext.Provider>
   );
--- client/src/components/HomeFeed/HomeFeed.js
+++ client/src/components/HomeFeed/HomeFeed.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { CurrentUserContext } from "../CurrentUserContext";
 
 export default function HomeFeed() {
+  const { feed, feedStatus } = React.useContext(CurrentUserContext);
+  if (feedStatus === "ok") {
+    console.log(feed);
+  }
   return (
     <div>
       <h1>Home</h1>
