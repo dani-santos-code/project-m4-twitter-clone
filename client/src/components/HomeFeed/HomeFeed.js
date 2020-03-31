94aa3d49498fc81cf6d7d34cd4e0848c08ed56bf
Dani Santos <santos.adaniele@gmail.com>
2020-03-31 16:59:07 -0400

    add avatar to tweets

--- client/src/components/HomeFeed/HomeFeed.js
+++ client/src/components/HomeFeed/HomeFeed.js
@@ -7,15 +7,34 @@ export default function HomeFeed() {
   const TweetUL = styled.ul`
     list-style: none;
   `;
+
+  const Avatar = styled.img`
+    border-radius: 50%;
+    width: 50px;
+    height: 50px;
+  `;
+  const TweetAuthor = styled.p`
+    font-weight: bold;
+  `;
+
+  const Status = styled.p``;
   return (
     <div>
       <h1>Home</h1>
-      {console.log(feed)}
       {feedStatus === "ok" ? (
         feed.tweetIds.map((tweet, i) => {
+          console.log(feed);
           return (
             <TweetUL>
-              <li key={`${i}-${tweet}`}>{feed.tweetsById[tweet].status}</li>
+              <li key={`${i}-${tweet}`}>
+                {feed.tweetsById[tweet].author && (
+                  <Avatar src={`${feed.tweetsById[tweet].author.avatarSrc}`} />
+                )}
+                <TweetAuthor>
+                  {feed.tweetsById[tweet].author.displayName}
+                </TweetAuthor>
+                <Status> {feed.tweetsById[tweet].status}</Status>
+              </li>
             </TweetUL>
           );
         })
