f05e529a1437dc3f5b0b8dfb5a3ef1c26c2cdeda
Dani Santos <santos.adaniele@gmail.com>
2020-04-01 09:53:34 -0400

    extract tweet component from HomeFeed

--- client/src/components/HomeFeed/HomeFeed.js
+++ client/src/components/HomeFeed/HomeFeed.js
@@ -1,46 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import { CurrentUserContext } from "../CurrentUserContext";
-
+import Tweet from "../Tweet";
 export default function HomeFeed() {
-  const { feed, feedStatus } = React.useContext(CurrentUserContext);
-  const TweetUL = styled.ul`
-    list-style: none;
-  `;
-
-  const Avatar = styled.img`
-    border-radius: 50%;
-    width: 50px;
-    height: 50px;
-  `;
-  const TweetAuthor = styled.p`
-    font-weight: bold;
-  `;
-
-  const Status = styled.p``;
   return (
     <div>
       <h1>Home</h1>
-      {feedStatus === "ok" ? (
-        feed.tweetIds.map((tweet, i) => {
-          console.log(feed);
-          return (
-            <TweetUL>
-              <li key={`${i}-${tweet}`}>
-                {feed.tweetsById[tweet].author && (
-                  <Avatar src={`${feed.tweetsById[tweet].author.avatarSrc}`} />
-                )}
-                <TweetAuthor>
-                  {feed.tweetsById[tweet].author.displayName}
-                </TweetAuthor>
-                <Status> {feed.tweetsById[tweet].status}</Status>
-              </li>
-            </TweetUL>
-          );
-        })
-      ) : (
-        <h1>Loading</h1>
-      )}
+      <Tweet />
     </div>
   );
 }
--- client/src/components/Tweet/Tweet.js
+++ client/src/components/Tweet/Tweet.js
@@ -1,5 +1,47 @@
 import React, { useEffect } from "react";
+import styled from "styled-components";
+import { CurrentUserContext } from "../CurrentUserContext";
+
+const TweetUL = styled.ul`
+  list-style: none;
+`;
+
+const Avatar = styled.img`
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+`;
+const TweetAuthor = styled.p`
+  font-weight: bold;
+`;
+
+const Status = styled.p``;
 
 export default function Tweet() {
-  return <div></div>;
+  const { feed, feedStatus } = React.useContext(CurrentUserContext);
+
+  return (
+    <>
+      {feedStatus === "ok" ? (
+        feed.tweetIds.map((tweet, i) => {
+          console.log(feed);
+          return (
+            <TweetUL>
+              <li key={`${i}-${tweet}`}>
+                {feed.tweetsById[tweet].author && (
+                  <Avatar src={`${feed.tweetsById[tweet].author.avatarSrc}`} />
+                )}
+                <TweetAuthor>
+                  {feed.tweetsById[tweet].author.displayName}
+                </TweetAuthor>
+                <Status> {feed.tweetsById[tweet].status}</Status>
+              </li>
+            </TweetUL>
+          );
+        })
+      ) : (
+        <h1>Loading</h1>
+      )}
+    </>
+  );
 }
